f2f21ebee56ec701ecfc8284b566d783c6e8e355
Alfonso Hooker <alfonso@tekperfect.com>
2023-11-30 14:53:08 -0800

    sample script for classroom lesson

--- alfonso-server-install.sh
+++ alfonso-server-install.sh
@@ -2,11 +2,10 @@
 
 echo -n "Would you like to install a Web Server or Database Server?... Enter 1 for Web or 2 for Database: "
 read VAR
-
-if [[ $VAR = 1 ]]
-      sudo apt install apache2 -y
-    fi
-  else
-    sudo apt install mysql-server-8.0 -y
-  fi
-}
+	if [[ $VAR = 1 ]]; then
+      		sudo apt install apache2 -y
+	fi
+	if [[ $VAR = 2 ]]; then
+    		sudo apt install mysql-server-8.0 -y
+	fi
+done
