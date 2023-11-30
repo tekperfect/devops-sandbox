7a505bd93aa74d7db6ba114ebcbe28d4ee8f6791
ulissesss <wareztown@inwind.it>
2023-11-30 12:17:19 -0800

    Install mysql/apache in ubuntu

---
+++ install_mysql_apache.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Install mysql or apache
+string1="mysql"
+string2="apache"
+echo -n "do you want install apache or mysql? (type: apache or mysql)"
+read choice
+case $choice in
+    $string1)
+sudo apt update -y && sudo apt install mysql-server -y && sudo systemctl start $
+;;
+    $string2)
+sudo apt update -y && sudo apt install apache2 -y
+;;
+esac
+
