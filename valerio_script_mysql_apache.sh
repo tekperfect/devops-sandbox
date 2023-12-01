2b5a51fb0ed22886e49ae79d4396ac6146e03391
ulissesss <wareztown@inwind.it>
2023-12-01 13:04:19 -0800

    cleared script

--- valerio_script_mysql_apache.sh
+++ valerio_script_mysql_apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Install mysql, apache, mongodb, postgres
+#Install mysql, apache, mongodb
 echo -n "do you want install 1) apache 2) mysql 3) mongodb 4)Postgres?"
 
 while true; do
@@ -12,7 +12,7 @@ elif [[ "$num" == '2' ]]; then
 sudo apt update -y && sudo apt install apache2 -y
 break
 elif [[ "$num" == '3' ]]; then
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyr                                       ings/mongodb-server-7.0.gpg --dearmor && echo "deb [ arch=amd64,arm64 signed-by                                       =/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubun                                       tu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb                                       -org-7.0.list && sudo apt update && sleep 10 && sudo apt install -y mongodb-org                                       ;
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor >
 break
 elif [[ "$num" == '4' ]]; then
 sudo apt update -y && sudo apt install postgresql-server-dev-all -y
