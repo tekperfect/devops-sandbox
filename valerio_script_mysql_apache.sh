73c0329a56ca1dce957180bf3857b66644db63dc
ulissesss <wareztown@inwind.it>
2023-12-01 13:07:43 -0800

    cleared script

--- valerio_script_mysql_apache.sh
+++ valerio_script_mysql_apache.sh
@@ -12,7 +12,7 @@ elif [[ "$num" == '2' ]]; then
 sudo apt update -y && sudo apt install apache2 -y
 break
 elif [[ "$num" == '3' ]]; then
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor >
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor && echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list && sudo apt update && sleep 10 && sudo apt install -y mongodb-org;
 break
 elif [[ "$num" == '4' ]]; then
 sudo apt update -y && sudo apt install postgresql-server-dev-all -y
