d14d42b7d11b2e2c8950d112443365c5e1f8ee36
alfonsoh <alfonso@tekperfect.com>
2023-12-01 19:08:42 -0800

    removing done

--- alfonso-server-install.sh
+++ alfonso-server-install.sh
@@ -14,4 +14,3 @@ read VAR
 	if [[ $VAR = 4 ]]; then
     		sudo apt install gnupg curl && curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list && sudo apt-get update && sudo apt-get install mongodb-org -y
 	fi
-done
