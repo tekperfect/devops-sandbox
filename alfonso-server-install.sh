11caacab3c2e321b3a2f7470ff3a9e847d0b20f4
alfonsoh <alfonso@tekperfect.com>
2023-12-01 18:51:09 -0800

    Fixing script for mongo install

--- alfonso-server-install.sh
+++ alfonso-server-install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo -n "Would you like to install a Web Server or Database Server?... Enter 1 for Web or 2 for Database: "
+echo -n "Would you like to install a Web Server or Database Server?... Enter 1 for Apache, 2 for MySQL, 3 for Postgres, or 4 for MongoDB: "
 read VAR
 	if [[ $VAR = 1 ]]; then
       		sudo apt install apache2 -y
 	fi
 	if [[ $VAR = 2 ]]; then
     		sudo apt install mysql-server-8.0 -y
+	fi
+	if [[ $VAR = 3 ]]; then
+    		sudo apt install postgresql-server-dev-all -y
+	fi
+	if [[ $VAR = 4 ]]; then
+    		sudo apt install gnupg curl && curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list && sudo apt-get update && sudo apt-get install mongodb-org -y
 	fi
 done
