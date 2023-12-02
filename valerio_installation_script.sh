288fe46e15e1269fc81867d755b361eab8e9312f
ulissesss <wareztown@inwind.it>
2023-12-02 00:38:49 -0800

    Added function to check if the softwares were previously installed.

--- valerio_installation_script.sh
+++ valerio_installation_script.sh
@@ -1,25 +1,37 @@
+
 #!/bin/bash
-#Install mysql, apache, mongodb
-echo -n "do you want install 1) apache 2) mysql 3) mongodb 4)Postgres?"
+#Install mysql, apache, mongodb, postgres
+
+checking (){
+ if ! [ -x "$(command -v '$1')" ]; then
+    echo "$1 already installed."
+    exit 1
+ fi
+}
+
+echo -n "do you want install 1) mysql 2) apache 3) mongodb 4)Postgres?"
 
 while true; do
 read num
 
    if [[ "$num" == '1' ]]; then
+checking "mysql"
 sudo apt update -y && sudo apt install mysql-server -y && sudo systemctl start
 break
 elif [[ "$num" == '2' ]]; then
+checking "apache2"
 sudo apt update -y && sudo apt install apache2 -y
 break
 elif [[ "$num" == '3' ]]; then
+checking "mongosh"
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor && echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list && sudo apt update && sleep 10 && sudo apt install -y mongodb-org;
 break
 elif [[ "$num" == '4' ]]; then
+checking "psql"
 sudo apt update -y && sudo apt install postgresql-server-dev-all -y
 break
-else 
+else
    echo "You did not enter a correct value"
 break
 fi
 done
-
