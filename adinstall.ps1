d34f6626745e3a4f72151c38b40cde69e313af5d
alfonsoh <alfonso@tekperfect.com>
2023-12-05 11:32:52 -0800

    fixing code for ad install

--- adinstall.ps1
+++ adinstall.ps1
@@ -7,4 +7,4 @@ get-windowsfeature
 install-windowsfeature AD-Domain-Services
 
 # This install AD with the specified paramaters
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath “C:\Windows\NTDS” -DomainMode “Win2012R2” -DomainName “domain_name.com” -DomainNetbiosName “DOMAIN_NAME” -ForestMode “Win2012R2” -InstallDns:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$false -SysvolPath “C:\Windows\SYSVOL” -Force:$true
+" -DomainNetbiosName "Server" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -String <password> -AsPlainText -Force)
