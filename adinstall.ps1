d4fdeb4f12a03ba90ed5748ca32a9532b5d984f4
alfonsoh <alfonso@tekperfect.com>
2023-12-05 11:22:03 -0800

    install ad with my settings

--- adinstall.ps1
+++ adinstall.ps1
@@ -1,3 +1,10 @@
 # This will prep the server for AD install
+
+# This gets the windows features that are available
 get-windowsfeature
+
+# This install ad domain services
 install-windowsfeature AD-Domain-Services
+
+# This install AD with the specified paramaters
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath “C:\Windows\NTDS” -DomainMode “Win2012R2” -DomainName “alfonso.net” -DomainNetbiosName “ALFONSO” -ForestMode “Win2012R2” -InstallDns:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$false -SysvolPath “C:\Windows\SYSVOL” -Force:$true
