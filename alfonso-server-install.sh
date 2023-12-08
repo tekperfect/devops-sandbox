6432e8e83211809dcfde5a073d5e9c97d8d876d6
alfonsoh <alfonso@tekperfect.com>
2023-12-08 10:01:26 -0800

    adding mongo install

--- alfonso-server-install.sh
+++ alfonso-server-install.sh
@@ -12,8 +12,5 @@ read VAR
     		sudo apt install postgresql-server-dev-all -y
 	fi
 	if [[ $VAR = 4 ]]; then
-    		sudo apt update -y && sudo apt install -y gnupg curl && curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor && echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list && sudo apt update && sleep 10 && sudo apt install -y mongodb-org
-	fi
-	if [[ $VAR != 4 ]]; then
-    		sudo apt update -y && sudo apt install -y gnupg curl && curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor && echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list && sudo apt update && sleep 10 && sudo apt install -y mongodb-org
+                sudo apt install mongodb-org -y
 	fi
